e3dc202cb4b14a67e0a9875a2fc44444347f3b87
Eugene Ostrouknov <eostroukhov@gmail.com>
2022-01-09 12:49:44 -0800

    Your new repo popped up on my feed, so I took a look.
    1. I changed to ES6 class. This way code is much easier to read.
       You are using WebPack so there's no reason to use prototypes.
    2. Take a look how I switched image loading to rely on promises.
       It should make it easier to handle errors and to reuse the code.
       Also note that I do not assign to onload/onerror - addEventListener
       is much cleaner and allows you to add multiple listeners to
       the same event.

--- src/AttackAnimation.js
+++ src/AttackAnimation.js
@@ -1,35 +1,37 @@
+class Animation {
+    constructor(min, max, frontSpeed, backSpeed) {
+        this.forward = true;
+        this.val = 0;
+        this.active = false;
+
+        this.max = max;
+        this.min = min;
+        this.fs = frontSpeed;
+        this.bs = backSpeed;
+    }
+
+    /**
+     * delta is time passed since the last frame
+     * 
+     * @param delta 
+     */
+    update(delta) {
+        console.log("val: " + this.val);
+        if (this.forward) {
+            this.val += delta * this.fs;
+            if (this.val >= this.max) {
+                this.val = this.max;
+                this.forward = false;
+            }
+        }
+        else {
+            this.val -= delta * this.bs;
+            if (this.val <= this.min) {
+                this.val = this.min;
+                this.forward = true;
+            }
+        }
+    }
+}
+
 module.exports = Animation;
-
-function Animation(min, max, frontSpeed, backSpeed) {
-    this.forward = true;
-    this.val = 0;
-    this.active = false;
-
-    this.max = max;
-    this.min = min;
-    this.fs = frontSpeed;
-    this.bs = backSpeed;
-}
-
-/**
- * delta is time passed since the last frame
- * 
- * @param delta 
- */
-Animation.prototype.update = function(delta) {
-    console.log("val: " + this.val);
-    if (this.forward) {
-        this.val += delta * this.fs;
-        if (this.val >= this.max) {
-            this.val = this.max;
-            this.forward = false;
-        }
-    }
-    else {
-        this.val -= delta * this.bs;
-        if (this.val <= this.min) {
-            this.val = this.min;
-            this.forward = true;
-        }
-    }
-}
